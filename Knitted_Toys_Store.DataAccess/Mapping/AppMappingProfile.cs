3a089055dd90effbe3e462f8c0f1d0f7ee3d58da
DevBioKir <DevBioKir@gmail.com>
2025-03-09 22:20:08 +0500

    Complete the creation of Repositories with methods for entities and connect to DB

---
+++ Knitted_Toys_Store.DataAccess/Mapping/AppMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Knitted_Toys_Store.DataAccess.Entities;
+using Knitted_Toys_Store.Domain.Models.Domain;
+
+namespace Knitted_Toys_Store.DataAccess.Mapping
+{
+    public class AppMappingProfile : Profile
+    {
+        public AppMappingProfile()
+        {
+            CreateMap<Toy, ToyEntity>().ReverseMap(); //первым передаем тип-источник значений, вторым – тип-приемник. Чтобы маппинг работал в обоих направлениях, используем .ReverseMap();
+        }
+    }
+}
