98c5e1f57ae277c0a3c377e8dfd90dace0233b2d
DevBioKir <DevBioKir@gmail.com>
2025-03-18 10:52:06 +0500

    Add Services

---
+++ Knitted_Toys_Store.API/Controllers/ToyController.cs
@@ -0,0 +1,16 @@
+﻿using Knitted_Toys_Store.App.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Knitted_Toys_Store.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ToyController : ControllerBase
+    {
+        private readonly IToyService
+        public ToyController(IToyService toyService)
+        {
+            
+        }
+    }
+}
