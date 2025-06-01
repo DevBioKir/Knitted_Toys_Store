47b6ca03b6145b65a95bea44b5016dd0230b77a4
DevBioKir <DevBioKir@gmail.com>
2025-06-01 15:36:43 +0500

    Changed NEXT_PUBLIC_DEV_API_BASE_URL

--- Frontend/knitted_toys_store/next.config.ts
+++ Frontend/knitted_toys_store/next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   env: {
-    NEXT_PUBLIC_DEV_API_BASE_URL: 'http://localhost/api',
-    //NEXT_PUBLIC_DEV_API_BASE_URL: 'http://nginx',
+    //NEXT_PUBLIC_DEV_API_BASE_URL: 'http://localhost/api',
+    NEXT_PUBLIC_DEV_API_BASE_URL: 'http://nginx',
   },
 };
 
