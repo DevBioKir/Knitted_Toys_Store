e038ae02fd38880d94ef3e31fb3e9967c7ae3e22
DevBioKir <DevBioKir@gmail.com>
2025-06-03 13:26:17 +0500

    Changed entrypoint

--- Backend/entrypoint.sh
+++ Backend/entrypoint.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "⏳ Ожидание готовности PostgreSQL..."
-
-# Ждём, пока БД будет доступна
-until dotnet ef database update --project Knitted_Toys_Store.API --startup-project Knitted_Toys_Store.API; do
-  >&2 echo "❌ База данных недоступна - повтор через 3 секунды..."
-  sleep 3
-done
-
-echo "✅ Миграции успешно применены!"
-
 # Запуск приложения
 exec dotnet Knitted_Toys_Store.API.dll
