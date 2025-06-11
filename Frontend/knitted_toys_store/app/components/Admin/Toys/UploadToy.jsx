5084ea75dd413396df09c3feb097cf8d626cff2d
DevBioKir <DevBioKir@gmail.com>
2025-06-11 16:25:10 +0500

    Cleaned up Frontend

--- Frontend/knitted_toys_store/app/components/Admin/Toys/UploadToy.jsx
+++ Frontend/knitted_toys_store/app/components/Admin/Toys/UploadToy.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./UploadPage.css"; // Путь к файлу стилей
+import "./UploadPage.css";
 import uploadToysFromExcel from "@/app/services/Admin/serviceUploadToysFromExcel";
 
 export const ToyUploadPage = () => {
